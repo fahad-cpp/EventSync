6cadcd8b7f1385349c7a7951f1047a9dc9995716
Fahad704 <shaikh.fahad.77377@gmail.com>
2025-09-05 14:26:37 +0530

    Modified script

--- startserver.bat
+++ startserver.bat
@@ -1,3 +1,9 @@
+@echo off
+if not exist node_modules start npm install && timeout /t 5
 start npm start
-start chrome "http://localhost:3000"
-if %errorlevel% NEQ 0 start duckduckgo "http://localhost:3000"
+where chrome
+if %errorlevel% NEQ 0 (
+	start duckduckgo "http://localhost:3000"
+) else (
+	start chrome ""http://localhost:3000"
+)
